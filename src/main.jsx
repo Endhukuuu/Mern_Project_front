f38564264e847d1e3d25592cde1957ee3433661c
Anand9898513 <arellianand5@gmail.com>
2026-06-12 22:51:54 +0530

    Fixed App import case issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from './app';
 import './css/Global.css'; // Professional global styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
